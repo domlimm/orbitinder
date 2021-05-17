865bfa60398d28a205abb64b9f16f11881216dec
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-17 21:57:38 +0800

    Imported Fragment instead

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { StatusBar } from 'react-native';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
@@ -7,13 +7,13 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 const App = () => {
   return (
-    <React.Fragment>
+    <Fragment>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={eva.light}>
         <StatusBar barStyle='default' style='auto' />
         <LandingNavigator />
       </ApplicationProvider>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
