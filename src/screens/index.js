845c2959c152c71c43aa1a18f310a173daaeb1d8
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-18 18:34:45 +0800

    Added parent export file for screens

---
+++ src/screens/index.js
@@ -0,0 +1,8 @@
+export { default as HomeLandingScreen } from './Startup/HomeLandingScreen';
+export { default as InputBackgroundScreen1 } from './Startup/InputBackgroundScreen1';
+export { default as InputBackgroundScreen2 } from './Startup/InputBackgroundScreen2';
+export { default as InputBackgroundScreen3 } from './Startup/InputBackgroundScreen3';
+export { default as SignupScreen } from './Auth/SignupScreen';
+export { default as LoginScreen } from './Auth/LoginScreen';
+export { default as LoginLandingScreen } from './Startup/LoginLandingScreen';
+export { default as ProfileLandingScreen } from './Startup/ProfileLandingScreen';
