d6e838476abced52748b2dbeaece29e7f16ee535
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-18 18:54:27 +0800

    - Added custom-theme.json - Refactored app's theme

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React, { Fragment } from 'react';
 import { StatusBar } from 'react-native';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
-import LandingNavigator from './src/navigation/LandingNavigator';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import LandingNavigator from './src/navigation/LandingNavigator';
+
+import { default as customTheme } from './src/constants/custom-theme.json';
 
 const App = () => {
   return (
     <Fragment>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
+      <ApplicationProvider {...eva} theme={{ ...eva.light, ...customTheme }}>
         <StatusBar barStyle='default' style='auto' />
         <LandingNavigator />
       </ApplicationProvider>
