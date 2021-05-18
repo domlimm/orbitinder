e34438815e12e15026ccb5bb31b843e64841cb10
Roopa Niedu <bitznobel@gmail.com>
2021-05-18 17:00:26 +0800

    created background screen 3

---
+++ src/screens/InputBackgroundScreen3.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Image, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import {
+  Button,
+  Layout,
+  Select,
+  SelectItem,
+  Text
+} from '@ui-kitten/components';
+import { ProfileHeader } from '../components/navigation/index';
+
+const InputBackgroundScreen3 = ({ navigation }) => {
+  const navigateDetails = () => {
+    console.log('btn pressed');
+  };
+
+  const [selectedIndex, setSelectedIndex] = React.useState([]);
+  const gameDevData = [
+    'Unity',
+    'Gamemaker',
+    'pygame',
+    'Unreal Engine',
+    'CryEngine',
+    'Amazon Lumberyard',
+    'Phaser'
+  ];
+  const displayGameDev = selectedIndex.map(index => {
+    console.log(index.row);
+    return gameDevData[index.row];
+  });
+  let navProps = {
+    navigation: navigation,
+    needBackNav: true
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ProfileHeader {...navProps} />
+      <Layout style={styles.textContainer}>
+        <Text style={styles.screenTitle}>Technology Experience</Text>
+        <Text style={styles.screenCaption}>
+          Let others know what you're great at!
+        </Text>
+      </Layout>
+      <Layout style={styles.inputContainer}>
+        <Select
+          label='Game Development'
+          style={styles.selectInput}
+          multiSelect={true}
+          selectedIndex={selectedIndex}
+          onSelect={index => setSelectedIndex(index)}
+          placeholder='Select'
+          value={displayGameDev.join(', ')}
+        >
+          {gameDevData.map((prop, key) => (
+            <SelectItem key={key} title={prop} />
+          ))}
+        </Select>
+      </Layout>
+      <Layout style={styles.btnContainer}>
+        <Button onPress={navigateDetails} style={styles.signupBtn}>
+          Next
+        </Button>
+      </Layout>
+      <StatusBar style='auto' />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  btnContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  inputContainer: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  textContainer: {
+    marginVertical: 20,
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '70%'
+  },
+  signupBtn: {
+    width: '70%',
+    marginVertical: 30,
+    backgroundColor: '#407BFF'
+  },
+  selectInput: {
+    width: '70%',
+    marginVertical: 10
+  },
+  screenTitle: {
+    color: '#407BFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+    width: '70%'
+  },
+  screenCaption: {
+    color: '#8cb0ff',
+    fontSize: 12,
+    fontWeight: 'bold',
+    width: '70%'
+  }
+});
+
+export default InputBackgroundScreen3;
