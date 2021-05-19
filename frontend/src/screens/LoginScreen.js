50ae7a4630352bf4df72d08618133aa2cad1481b
Roopa Niedu <bitznobel@gmail.com>
2021-05-19 16:26:56 +0800

    Added keyboardavoidingview and scrollview to log in form

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet
+} from 'react-native';
 import { Button, Layout, Input } from '@ui-kitten/components';
 import { LandingHeader, LandingImage } from '../components/navigation/index';
 import AuthHeader from '../components/navigation/BackTopNav';
@@ -14,29 +21,35 @@ const LoginScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.parentContainer}>
       <AuthHeader navigation={navigation} />
-      <Layout style={styles.landingImageContainer}>
-        <LandingImage imgSrc={require('../assets/images/high-five-pana.png')} />
-      </Layout>
-      <Layout style={styles.inputContainer}>
-        <Input
-          label='Email'
-          style={styles.textInput}
-          placeholder='example@mail.com'
-          value={value}
-          onChangeText={nextValue => setValue(nextValue)}
-        />
-        <Input
-          label='Password'
-          style={styles.textInput}
-          placeholder='********'
-        />
-        <Button
-          onPress={() => console.log('Login Btn Pressed')}
-          style={styles.loginBtn}
-        >
-          Log In
-        </Button>
-      </Layout>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior='height'>
+        <ScrollView>
+          <Layout style={styles.landingImageContainer}>
+            <LandingImage
+              imgSrc={require('../assets/images/high-five-pana.png')}
+            />
+          </Layout>
+          <Layout style={styles.inputContainer}>
+            <Input
+              label='Email'
+              style={styles.textInput}
+              placeholder='example@mail.com'
+              value={value}
+              onChangeText={nextValue => setValue(nextValue)}
+            />
+            <Input
+              label='Password'
+              style={styles.textInput}
+              placeholder='********'
+            />
+            <Button
+              onPress={() => console.log('Login Btn Pressed')}
+              style={styles.loginBtn}
+            >
+              Log In
+            </Button>
+          </Layout>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -47,7 +60,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white'
   },
   landingImageContainer: {
-    height: '50%'
+    height: Dimensions.get('window').height / 2
   },
   inputContainer: {
     height: '50%',
