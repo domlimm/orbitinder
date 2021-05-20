feb48f38fc4cf010cc16aaeee14e8920961b1a96
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-21 01:45:19 +0800

    Amended Sign Up screen UI

--- frontend/src/screens/Auth/SignupScreen.js
+++ frontend/src/screens/Auth/SignupScreen.js
@@ -7,39 +7,54 @@ import {
   Input,
   Select,
   SelectItem,
-  IndexPath
+  IndexPath,
+  Icon,
+  Text
 } from '@ui-kitten/components';
 // To separate for local imports rather than installed dependencies: add below onwards
 import { AuthHeader } from '../../components/index';
 
 const SignupScreen = ({ navigation }) => {
-  const navigateDetails = () => {
-    navigation.navigate('ProfileLanding');
-  };
-
-  const [emailValue, setValue] = React.useState('');
-
+  const [fName, setFName] = React.useState('');
+  const [lName, setLName] = React.useState('');
+  const [email, setEmail] = React.useState('');
   const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
   const genderData = ['Male', 'Female'];
   const displayValue = genderData[selectedIndex.row];
 
+  const navigateDetails = () => {
+    navigation.navigate('ProfileLanding');
+  };
+
+  const NameIcon = props => <Icon {...props} name='smiling-face-outline' />;
+  const EmailIcon = props => <Icon {...props} name='email-outline' />;
+  const PasswordIcon = props => <Icon {...props} name='lock-outline' />;
+
+  const Title = ({ title }) => (
+    <Layout style={styles.titleContainer}>
+      <Text style={styles.title}>{title.toUpperCase()}</Text>
+    </Layout>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView style={styles.formContainer}>
+        <AuthHeader navigation={navigation} />
         <ScrollView>
-          <AuthHeader navigation={navigation} />
           <Layout style={styles.inputContainer}>
+            <Title title='Personal Details' />
             <Input
               label='First Name'
               style={styles.textInput}
               placeholder='John'
+              accessoryRight={NameIcon}
             />
             <Input
               label='Last Name'
               style={styles.textInput}
               placeholder='Doe'
+              accessoryRight={NameIcon}
             />
-
             <Select
               style={styles.selectInput}
               selectedIndex={selectedIndex}
@@ -51,18 +66,21 @@ const SignupScreen = ({ navigation }) => {
                 <SelectItem key={key} title={value} />
               ))}
             </Select>
-
+            <Title title='Account' />
             <Input
               label='Email'
               style={styles.textInput}
               placeholder='example@mail.com'
-              value={emailValue}
-              onChangeText={nextValue => setValue(nextValue)}
+              value={email}
+              onChangeText={input => setEmail(input)}
+              accessoryRight={EmailIcon}
+              keyboardType='email-address'
             />
             <Input
               label='Password'
               style={styles.textInput}
               placeholder='********'
+              accessoryRight={PasswordIcon}
             />
           </Layout>
           <Layout style={styles.btnContainer}>
@@ -84,6 +102,15 @@ const styles = StyleSheet.create({
   formContainer: {
     flex: 1
   },
+  titleContainer: {
+    width: '70%',
+    marginTop: 10
+  },
+  title: {
+    color: '#407BFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
   btnContainer: {
     flex: 1,
     alignItems: 'center'
@@ -95,14 +122,15 @@ const styles = StyleSheet.create({
   },
   textInput: {
     width: '70%',
-    marginBottom: 20
+    marginVertical: 10
   },
   signupBtn: {
     width: '70%',
     marginVertical: 30
   },
   selectInput: {
-    width: '70%'
+    width: '70%',
+    marginVertical: 10
   },
   screenTitle: {
     color: '#407BFF',
