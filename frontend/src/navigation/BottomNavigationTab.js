a0f3326c78985e443ca8c7e67f75f66fdc4964fd
Roopa Niedu <bitznobel@gmail.com>
2021-05-21 18:45:37 +0800

    Created Bottom Navigator

---
+++ frontend/src/navigation/BottomNavigationTab.js
@@ -0,0 +1,46 @@
+// import { ActivityIcon, VideoIcon } from '../assets/icons';
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Divider,
+  Icon
+} from '@ui-kitten/components';
+import React from 'react';
+import { View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  MainAppScreen,
+  ChatOverviewScreen,
+  TeamUpScreen
+} from '../screens/index';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const HomeIcon = props => <Icon {...props} name='home-outline' />;
+
+const ChatIcon = props => <Icon {...props} name='message-square-outline' />;
+
+const TeamUpIcon = props => <Icon {...props} name='person-outline' />;
+
+const BottomTabBar = ({ navigation, state }) => (
+  <View>
+    <Divider />
+    <BottomNavigation
+      appearance='noIndicator'
+      selectedIndex={state.index}
+      onSelect={index => navigation.navigate(state.routeNames[index])}
+    >
+      <BottomNavigationTab icon={HomeIcon} />
+      <BottomNavigationTab icon={TeamUpIcon} />
+      <BottomNavigationTab icon={ChatIcon} />
+    </BottomNavigation>
+  </View>
+);
+
+export const BottomTabsNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name='Home' component={MainAppScreen} />
+    <Screen name='TeamUpScreen' component={TeamUpScreen} />
+    <Screen name='ChatOverviewScreen' component={ChatOverviewScreen} />
+  </Navigator>
+);
---
+++ frontend/src/screens/Main/TeamUpScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, RefreshControl, ScrollView } from 'react-native';
+import { Layout, Text } from '@ui-kitten/components';
+import { SafeAreaView } from 'react-native-safe-area-context';
+// To separate for local imports rather than installed dependencies: add below onwards
+import { TitleHeader } from '../../components/index';
+
+const TeamUpScreen = ({ navigation }) => {
+  let navProps = {
+    title: 'Team Up',
+    navigation: navigation,
+    needBackNav: false,
+    needMenuNav: true
+  };
+  return (
+    <SafeAreaView style={styles.parentContainer}>
+      <TitleHeader navProps={navProps} />
+      <ScrollView>
+        <Text>Team Up Screen</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    backgroundColor: '#F5F5F5'
+  }
+});
+
+export default TeamUpScreen;
