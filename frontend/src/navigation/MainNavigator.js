b5fc64f0f63ec3d41d0ffbb0bcec67db84f1050f
Roopa Niedu <bitznobel@gmail.com>
2021-05-21 11:55:50 +0800

    Created Drawer Navigator

--- frontend/src/navigation/MainNavigator.js
+++ frontend/src/navigation/MainNavigator.js
@@ -4,14 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ActivityFeedScreen from '../screens/Main/ActivityFeedScreen';
 import UserProfileScreen from '../screens/Main/UserProfileScreen';
 import MainAppScreen from '../screens/Main/MainAppScreen';
+import { HomeDrawerNavigator } from './DrawerNavigator';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const MainNavigator = () => (
   <Navigator headerMode='none'>
-    <Screen name='MainAppScreen' component={MainAppScreen} />
+    {/* <Screen name='MainAppScreen' component={MainAppScreen} />
     <Screen name='ActivityFeedScreen' component={ActivityFeedScreen} />
-    <Screen name='UserProfileScreen' component={UserProfileScreen} />
+    <Screen name='UserProfileScreen' component={UserProfileScreen} /> */}
+    <Screen name='DrawerNavigator' component={HomeDrawerNavigator} />
   </Navigator>
 );
 
