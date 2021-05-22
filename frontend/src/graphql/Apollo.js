9079d6345da96300a2a2afab0847382ee0f5021f
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-22 23:23:48 +0800

    Added authorization header

--- frontend/src/graphql/Apollo.js
+++ frontend/src/graphql/Apollo.js
@@ -1,22 +1,39 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Network from 'expo-network';
 
 const IP_ADDRESS = '192.168.1.163';
 const PORT = '4000';
 
-// const URI = `http://${IP_ADDRESS}:${PORT}/`;
+const URI = `http://${IP_ADDRESS}:${PORT}/`;
 
-const URI = async () => {
-  Network.getIpAddressAsync()
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
+// const URI = async () => {
+//   Network.getIpAddressAsync()
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// };
+
+const httpLink = createHttpLink({
+  uri: URI
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await AsyncStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token || ''
+    }
+  };
+});
 
 export const client = new ApolloClient({
-  uri: `http://${IP_ADDRESS}:${PORT}/`,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
