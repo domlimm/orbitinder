d8b2f5c307b973482bfb543bc8545aa1db85fb3c
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-26 17:59:35 +0800

    - Added more imports for preloading of images - Reorganized imports alphabetically

--- frontend/src/constants/imagePaths.js
+++ frontend/src/constants/imagePaths.js
@@ -1,4 +1,7 @@
 export const localImages = [
+  require('../assets/images/edit.png'),
+  require('../assets/images/email-img.png'),
+  require('../assets/images/forgot-password-img.png'),
   require('../assets/images/high-five-pana.png'),
   require('../assets/images/login-image.png'),
   require('../assets/images/login-image-2.png'),
@@ -6,7 +9,6 @@ export const localImages = [
   require('../assets/images/orbital-logo.png'),
   require('../assets/images/pref-landing-img.png'),
   require('../assets/images/profile-interface.png'),
-  require('../assets/images/splash.png'),
-  require('../assets/images/email-img.png'),
-  require('../assets/images/forgot-password-img.png')
+  require('../assets/images/save-icon.png'),
+  require('../assets/images/splash.png')
 ];
