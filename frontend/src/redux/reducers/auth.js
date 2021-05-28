264d46c63b76481a331bb99566eb485cde57f516
domsterthebot <dominiclimkj3448@gmail.com>
2021-05-29 00:48:53 +0800

    - Added unsubscription - Added field to state for action in redux

--- frontend/src/redux/reducers/auth.js
+++ frontend/src/redux/reducers/auth.js
@@ -1,6 +1,7 @@
 import { GET_USER_NAME } from '../actions/auth';
 
 const initialState = {
+  id: '',
   name: ''
 };
 
@@ -9,6 +10,7 @@ export default (state = initialState, action) => {
     case GET_USER_NAME:
       return {
         ...state,
+        id: action.id,
         name: action.user
       };
     default:
