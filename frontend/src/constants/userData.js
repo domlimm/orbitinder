36aeed11a4009d4f34c97765c782c635a820163b
Roopa Niedu <bitznobel@gmail.com>
2021-05-30 11:09:34 +0800

    Added User Data constants

--- frontend/src/constants/userData.js
+++ frontend/src/constants/userData.js
@@ -3,8 +3,6 @@ export const userData = {
   year: 'Year 1',
   major: 'Business Analytics',
   gender: 'Female',
-  linkedIn: 'https://www.linkedin.com/in/andrewgregovic',
-  github: 'https://github.com/roopaniedu',
   bio: 'Rebecca Renee Black is an American YouTuber and singer who gained  extensive media coverage when the music video for her 2011 single Friday went viral on YouTube and other social media sites. Friday was derided by many music critics and viewers, who dubbed it the worst song ever',
   idea: ['No'],
   level: ['Artemis'],
@@ -36,3 +34,106 @@ export const partnerPref = {
     db: ['Oracle']
   }
 };
+
+export const userArrayData = [
+  {
+    name: 'Rebecca Black',
+    year: 'Year 1',
+    major: 'Business Analytics',
+    gender: 'Female',
+    bio: 'Total problem solver. Professional thinker. Twitter fan. Freelance entrepreneur. Bacon enthusiast. Committed to doing the very best and works very hard. Looking for like minded people',
+    idea: ['No'],
+    level: ['Artemis'],
+    interestedAreas: ['Finance', 'Social'],
+    codingExpLevel: ['Beginner'],
+    commitment: ['High Commitment'],
+    technologies: ['Flutter/Dart', 'Android/Java', 'React-Native/Js', 'NLTK'],
+    tech: {
+      ml: ['NLTK'],
+      gamedev: ['Unity', 'Unreal Engine'],
+      webdev: ['React'],
+      mobiledev: ['Ionic', 'Corona'],
+      db: ['Oracle']
+    }
+  },
+  {
+    name: 'Bernard Richard',
+    year: 'Year 3',
+    major: 'Computer Science',
+    gender: 'Male',
+    bio: 'Evil reader. Coffee ninja. Infuriatingly humble bacon aficionado. Introvert. Typical entrepreneur. Amateur travel maven. Troublemaker. Pop culture junkie.',
+    idea: ['No'],
+    level: ['Gemini'],
+    interestedAreas: ['Finance', 'Games'],
+    codingExpLevel: ['Advanced'],
+    commitment: ['High Commitment'],
+    technologies: ['Flutter/Dart', 'Android/Java', 'React-Native/Js', 'NLTK'],
+    tech: {
+      ml: ['NLTK'],
+      gamedev: ['Unity', 'Unreal Engine'],
+      webdev: ['React'],
+      mobiledev: ['Ionic', 'Corona'],
+      db: ['Oracle']
+    }
+  },
+  {
+    name: 'Alisia Charlene',
+    year: 'Year 2',
+    major: 'Information Systems',
+    gender: 'Female',
+    bio: 'Creator. Certified travel aficionado. Gamer. Alcohol practitioner. Social media lover. Wannabe coffee nerd. Tv geek. ',
+    idea: ['No'],
+    level: ['Vostok'],
+    interestedAreas: ['Games', 'Mobile'],
+    codingExpLevel: ['Beginner'],
+    commitment: ['High Commitment'],
+    technologies: ['Flutter/Dart', 'Android/Java', 'React-Native/Js', 'NLTK'],
+    tech: {
+      ml: ['NLTK'],
+      gamedev: ['Unity', 'Unreal Engine'],
+      webdev: ['React'],
+      mobiledev: ['Ionic', 'Corona'],
+      db: ['Oracle']
+    }
+  },
+  {
+    name: 'Kev Christiana',
+    year: 'Year 1',
+    major: 'Computer Science',
+    gender: 'Female',
+    bio: 'Hipster-friendly analyst. Friend of animals everywhere. Alcohol buff. Social media scholar. Wannabe tv fan. Creator',
+    idea: ['Yes'],
+    level: ['Apollo'],
+    interestedAreas: ['Social'],
+    codingExpLevel: ['Beginner'],
+    commitment: ['High Commitment'],
+    technologies: ['Flutter/Dart', 'Android/Java', 'React-Native/Js', 'NLTK'],
+    tech: {
+      ml: ['NLTK'],
+      gamedev: ['Unity', 'Unreal Engine'],
+      webdev: ['React'],
+      mobiledev: ['Ionic', 'Corona'],
+      db: ['Oracle']
+    }
+  },
+  {
+    name: 'Pen Lenora',
+    year: 'Year 1',
+    major: 'Computer Engineering',
+    gender: 'Female',
+    bio: 'witter specialist. Music trailblazer. General social media evangelist. Internet aficionado. Web scholar.',
+    idea: ['Yes'],
+    level: ['Artemis'],
+    interestedAreas: ['Others'],
+    codingExpLevel: ['Advanced'],
+    commitment: ['Medium Commitment'],
+    technologies: ['Flutter/Dart', 'Android/Java', 'React-Native/Js', 'NLTK'],
+    tech: {
+      ml: ['NLTK'],
+      gamedev: ['Unity', 'Unreal Engine'],
+      webdev: ['React'],
+      mobiledev: ['Ionic', 'Corona'],
+      db: ['Oracle']
+    }
+  }
+];
