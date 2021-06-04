3e485deb41bc8994df7b6a4a98913d9ea338f3c4
domsterthebot <dominiclimkj3448@gmail.com>
2021-06-04 12:05:13 +0800

    Removed unused folders and codes (previously from segregation)

--- frontend/src/redux-toolkit/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import authReducer from './features/authSlice';
-
-export default configureStore({
-  reducer: {
-    auth: authReducer
-  }
-});
