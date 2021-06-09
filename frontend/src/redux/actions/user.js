e055a14376297d562f054f7a91c6d1dfc79cc220
domsterthebot <dominiclimkj3448@gmail.com>
2021-06-10 00:14:38 +0800

    - Added upload profile photo backend - Added uuid dependency for random profile photo name

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -1,4 +1,5 @@
 import firebase from '../../firebase';
+import uuid from 'uuid';
 
 export const ADD_USER_PROFILE = 'ADD_USER_PROFILE';
 export const ADD_USER_PREFERENCES = 'ADD_USER_PREFERENCES';
@@ -8,6 +9,87 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const UPDATE_PREFERENCES = 'UPDATE_PREFERENCES';
 
 const db = firebase.firestore();
+
+export const getUserData = () => dispatch => {
+  const userId = firebase.auth().currentUser.uid;
+
+  db.collection('users')
+    .doc(userId)
+    .get()
+    .then(res => {
+      dispatch({ type: GET_USER_DATA, userData: res.data() });
+    })
+    .catch(err => {
+      throw new Error(`Get User Data: ${err}`);
+    });
+};
+
+export const addProfile = data => dispatch => {
+  const userId = firebase.auth().currentUser.uid;
+
+  addProfilePhoto(data.imagePath).then(imagePath => {
+    db.collection('users')
+      .doc(userId)
+      .set(data)
+      .then(() => {
+        dispatch({
+          type: ADD_USER_PROFILE,
+          userData: { ...data, imagePath: imagePath }
+        });
+      })
+      .catch(err => {
+        throw new Error(`Adding Profile: ${err}`);
+      });
+  });
+};
+
+export const addPreferences = data => dispatch => {
+  const userId = firebase.auth().currentUser.uid;
+
+  db.collection('users')
+    .doc(userId)
+    .set(data, { merge: true })
+    .then(() => {
+      dispatch({ type: ADD_USER_PREFERENCES, userData: data });
+    })
+    .catch(err => {
+      throw new Error(`Adding Preferences: ${err}`);
+    });
+};
+
+export const addProfilePhoto = async uri => {
+  const userId = firebase.auth().currentUser.uid;
+  const metadata = {
+    contentType: 'image/jpeg'
+  };
+  const photoName = uuid.v4() + '.jpg';
+
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+
+    xhr.onerror = e => {
+      console.log(`Add Profile Photo: ${e}`);
+      reject(new TypeError('Network request failed'));
+    };
+
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
+  const storageRef = firebase
+    .storage()
+    .ref()
+    .child(`/users/${userId}/${photoName}`);
+  const snapshot = await storageRef.put(blob);
+
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
+};
 
 export const updatePref = data => dispatch => {
   const userId = firebase.auth().currentUser.uid;
@@ -37,52 +119,10 @@ export const updateProfile = data => dispatch => {
     });
 };
 
-export const addProfile = data => dispatch => {
-  const userId = firebase.auth().currentUser.uid;
-
-  db.collection('users')
-    .doc(userId)
-    .set(data)
-    .then(() => {
-      dispatch({ type: ADD_USER_PROFILE, userData: data });
-    })
-    .catch(err => {
-      throw new Error(`Adding Profile: ${err}`);
-    });
-};
-
-export const addPreferences = data => dispatch => {
-  const userId = firebase.auth().currentUser.uid;
-
-  db.collection('users')
-    .doc(userId)
-    .set(data, { merge: true })
-    .then(() => {
-      dispatch({ type: ADD_USER_PREFERENCES, userData: data });
-    })
-    .catch(err => {
-      throw new Error(`Adding Preferences: ${err}`);
-    });
-};
-
 export const clearDataLogOut = () => dispatch => {
   try {
     dispatch({ type: CLEAR_LOG_OUT });
   } catch (err) {
     throw new Error(`Clear Data: ${err}`);
   }
-};
-
-export const getUserData = () => dispatch => {
-  const userId = firebase.auth().currentUser.uid;
-
-  db.collection('users')
-    .doc(userId)
-    .get()
-    .then(res => {
-      dispatch({ type: GET_USER_DATA, userData: res.data() });
-    })
-    .catch(err => {
-      throw new Error(`Get User Data: ${err}`);
-    });
 };
