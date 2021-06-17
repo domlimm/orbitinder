939e7e3ac78fcb110e499584e459df6d3eef32e2
domsterthebot <dominiclimkj3448@gmail.com>
2021-06-17 12:07:23 +0800

    Added UI for p2p chats

--- frontend/src/screens/Main/ChatScreen.js
+++ frontend/src/screens/Main/ChatScreen.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import { StyleSheet, KeyboardAvoidingView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Layout, Text } from '@ui-kitten/components';
+import React, { useState, useCallback, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Layout, Divider } from '@ui-kitten/components';
+import { GiftedChat, Send, Bubble } from 'react-native-gifted-chat';
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { ChatHeader } from '../../components/index';
 
 const ChatScreen = ({ navigation, route }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        }
+      }
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  const scrollToBottomComponent = () => (
+    <Feather name='chevrons-down' size={24} color='#333' />
+  );
+
+  const renderSend = props => (
+    <Send {...props}>
+      <View>
+        <MaterialCommunityIcons
+          name='send'
+          style={styles.sendIcon}
+          size={24}
+          color='#407BFF'
+        />
+      </View>
+    </Send>
+  );
+
+  const renderBubble = props => (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: { backgroundColor: '#407BFF' }
+      }}
+    />
+  );
+
   const navProps = {
     navigation: navigation,
     name: route.params.data.name,
@@ -13,23 +63,33 @@ const ChatScreen = ({ navigation, route }) => {
   };
 
   return (
-    <KeyboardAvoidingView
-      style={styles.kbContainer}
-      behavior={Platform.OS === 'ios' ? 'padding' : null}
-    >
-      <SafeAreaView>
-        <ChatHeader navProps={navProps} />
-        <Layout>
-          <Text>This will be the 1 to 1 chat screen.</Text>
-        </Layout>
-      </SafeAreaView>
-    </KeyboardAvoidingView>
+    <Layout style={styles.mainContainer}>
+      <ChatHeader navProps={navProps} />
+      <Divider />
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+        scrollToBottom
+        scrollToBottomComponent={scrollToBottomComponent}
+        renderSend={renderSend}
+        alwaysShowSend
+        renderBubble={renderBubble}
+        renderAvatar={null}
+      />
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  kbContainer: {
+  mainContainer: {
     flex: 1
+  },
+  sendIcon: {
+    marginBottom: 10,
+    marginRight: 5
   }
 });
 
