84a44573adfcebbabcd9785b683af77fe541528a
domsterthebot <dominiclimkj3448@gmail.com>
2021-06-22 10:31:15 +0800

    - Added chat requests listener to watch for changes in chat requests for displaying - Added prop to FlatList to trigger re-render when chat requests is modified - Added handler to show empty chats

--- frontend/src/screens/Main/RequestsScreen.js
+++ frontend/src/screens/Main/RequestsScreen.js
@@ -1,34 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Layout } from '@ui-kitten/components';
+import { Layout, Text } from '@ui-kitten/components';
 import { useSelector } from 'react-redux';
+import firebase from '../../firebase/index';
 
 import { RequestItem } from '../../components/index';
 
 const RequestsScreen = () => {
-  // Temporary
-  const users = useSelector(state => state.users.usersData);
+  const userData = useSelector(state => state.user.userData);
+  const usersData = useSelector(state => state.users.usersData);
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    const latestReqsListener = firebase
+      .firestore()
+      .collection('users')
+      .doc(firebase.auth().currentUser.uid)
+      .onSnapshot(querySnapshot => {
+        const userRequests = querySnapshot.data().likedBy;
+        setRequests(userRequests);
+      });
+
+    return () => latestReqsListener();
+  }, []);
 
   return (
     <SafeAreaView style={styles.parentContainer}>
       <Layout style={styles.chatsContainer}>
-        <FlatList
-          style={styles.requestContainer}
-          data={users}
-          renderItem={({ item }) => (
-            <RequestItem
-              userData={item}
-              name={item.name}
-              imagePath={item.imagePath}
-              year={item.background.year}
-              degree={item.background.degree}
-              biography={item.background.biography}
-            />
-          )}
-          keyExtractor={item => item.id}
-          showsVerticalScrollIndicator={false}
-        />
+        {requests?.length > 0 ? (
+          <FlatList
+            style={styles.requestContainer}
+            data={requests}
+            renderItem={({ item }) => {
+              const senderData = usersData.filter(data => data.id === item)[0];
+
+              return (
+                <RequestItem
+                  userData={senderData}
+                  name={senderData.name}
+                  imagePath={senderData.imagePath}
+                  year={senderData.background.year}
+                  degree={senderData.background.degree}
+                  biography={senderData.background.biography}
+                />
+              );
+            }}
+            keyExtractor={item => item}
+            showsVerticalScrollIndicator={false}
+            extraData={requests}
+          />
+        ) : (
+          <Layout style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>
+              {`Sorry, we can't find any requests just yet.`}
+            </Text>
+          </Layout>
+        )}
       </Layout>
     </SafeAreaView>
   );
@@ -45,6 +73,16 @@ const styles = StyleSheet.create({
   requestContainer: {
     flex: 1,
     width: '100%'
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  emptyText: {
+    marginHorizontal: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 });
 
