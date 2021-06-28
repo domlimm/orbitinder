da5580c17a88c09b4074b54498cdabd2dfcdf379
domsterthebot <dominiclimkj3448@gmail.com>
2021-06-28 17:39:49 +0800

    Updated App test file

--- frontend/__tests__/App.test.js
+++ frontend/__tests__/App.test.js
@@ -1,11 +1,10 @@
+import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
-
 import App from '../App';
 
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
 });
