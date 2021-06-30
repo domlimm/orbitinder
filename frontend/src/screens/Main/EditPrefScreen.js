973d2aea66d17f2941f66bb58bc7398852ae72a4
domsterthebot <dominiclimkj3448@gmail.com>
2021-06-30 23:09:17 +0800

    Changed alert UI

--- frontend/src/screens/Main/EditPrefScreen.js
+++ frontend/src/screens/Main/EditPrefScreen.js
@@ -220,14 +220,14 @@ const EditPrefScreen = ({ navigation }) => {
             'Discard changes?',
             'You have unsaved changes. Are you sure to discard them and leave the screen?',
             [
-              { text: "Don't leave", style: 'cancel', onPress: () => {} },
               {
                 text: 'Discard',
                 style: 'destructive',
                 // If the user confirmed, then we dispatch the action we blocked earlier
                 // This will continue the action that had triggered the removal of the screen
                 onPress: () => navigation.dispatch(e.data.action)
-              }
+              },
+              { text: 'Stay', style: 'cancel', onPress: () => {} }
             ]
           );
         }
