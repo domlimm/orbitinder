c3e53820fe89e722b56b5708ae0ede75b24cdeed
domsterthebot <dominiclimkj3448@gmail.com>
2021-07-02 12:23:37 +0800

    Reorganise screen index for readability

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,30 +1,33 @@
 export { default as HomeLandingScreen } from './Landing/HomeLandingScreen';
 export { default as OnboardingScreen } from './Landing/OnboardingScreen';
+export { default as LoginLandingScreen } from './Landing/LoginLandingScreen';
+export { default as ProfileLandingScreen } from './Landing/ProfileLandingScreen';
+export { default as PreferencesLandingScreen } from './Landing/PreferencesLandingScreen';
+
 export { default as InputProfilePhotoScreen } from './Registration/InputProfilePhotoScreen';
 export { default as InputBackgroundScreen1 } from './Registration/InputBackgroundScreen1';
 export { default as InputBackgroundScreen2 } from './Registration/InputBackgroundScreen2';
 export { default as InputBackgroundScreen3 } from './Registration/InputBackgroundScreen3';
-export { default as SignupScreen } from './Auth/SignupScreen';
-export { default as LoginScreen } from './Auth/LoginScreen';
-export { default as LoginLandingScreen } from './Landing/LoginLandingScreen';
-export { default as ProfileLandingScreen } from './Landing/ProfileLandingScreen';
-export { default as PreferencesLandingScreen } from './Landing/PreferencesLandingScreen';
 export { default as PrefInputScreen1 } from './Registration/PreferenceInputScreen1';
 export { default as PrefInputScreen2 } from './Registration/PreferenceInputScreen2';
+
+export { default as SignupScreen } from './Auth/SignupScreen';
+export { default as LoginScreen } from './Auth/LoginScreen';
+export { default as ForgotPasswordScreen } from './Auth/ForgotPasswordScreen';
+export { default as ForgotPasswordConfirmationScreen } from './Auth/ForgotPasswordConfirmationScreen';
 export { default as ActivityFeedScreen } from './Main/ActivityFeedScreen';
 export { default as UserProfileScreen } from './Main/UserProfileScreen';
 export { default as MainAppScreen } from './Main/MainAppScreen';
 export { default as ChangePasswordScreen } from './Main/ChangePasswordScreen';
-export { default as ErrorScreen } from './Miscellaneous/ErrorScreen';
-export { default as ForgotPasswordScreen } from './Auth/ForgotPasswordScreen';
-export { default as ForgotPasswordConfirmationScreen } from './Auth/ForgotPasswordConfirmationScreen';
 export { default as ChatsOverviewScreen } from './Main/ChatsOverviewScreen';
 export { default as TeamUpScreen } from './Main/TeamUpScreen';
 export { default as EditProfileScreen } from './Main/EditProfileScreen';
 export { default as TeamUpProfileScreen } from './Main/TeamupProfileScreen';
 export { default as RecoUserScreen } from './Main/RecoUserScreen';
-export { default as LoadingScreen } from './Miscellaneous/LoadingScreen';
 export { default as UserPreferencesScreen } from './Main/UserPreferencesScreen';
 export { default as EditPrefScreen } from './Main/EditPrefScreen';
 export { default as ChatScreen } from './Main/ChatScreen';
 export { default as RequestsScreen } from './Main/RequestsScreen';
+
+export { default as ErrorScreen } from './Miscellaneous/ErrorScreen';
+export { default as LoadingScreen } from './Miscellaneous/LoadingScreen';
