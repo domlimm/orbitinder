b0e2acc7a0c0fc8ae9b9fbf9454ddb9fbd138111
domsterthebot <dominiclimkj3448@gmail.com>
2021-07-03 19:31:30 +0800

    Added team up status card

--- frontend/src/components/Dashboard/Status.js
+++ frontend/src/components/Dashboard/Status.js
@@ -1,37 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Image, Platform } from 'react-native';
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout, Text, Avatar } from '@ui-kitten/components';
 import dayjs from 'dayjs';
+import { useSelector } from 'react-redux';
+
+import { UserAvatar } from '..';
 
 const Status = () => {
+  const [match, setMatch] = useState(false);
+  const [myImage, setMyImage] = useState('');
+  const [teammate, setTeammate] = useState({});
+  const [teamName, setTeamName] = useState('');
+  const [teamImage, setTeamImage] = useState('');
+
+  const { name } = useSelector(state => state.auth);
+  const userData = useSelector(state => state.user.userData);
+  const usersData = useSelector(state => state.users.usersData);
   const todayDate = dayjs(new Date()).format('D MMMM YY');
 
+  useEffect(() => {
+    if (userData.matchId !== undefined) {
+      setMatch(userData.matchId.length > 0);
+    }
+
+    if (userData.imagePath !== undefined) {
+      setMyImage(userData.imagePath);
+    }
+
+    if (userData.matchId !== undefined && usersData !== undefined) {
+      const matchId = userData.matchId;
+
+      if (matchId.length > 0) {
+        const match = usersData.filter(user => user.id === matchId)[0];
+
+        setTeammate(match);
+        setTeamName(match.name);
+        setTeamImage(match.imagePath);
+      }
+    }
+  }, []);
+
+  const Connector = () => <View style={styles.connector} />;
+
   return (
-    <Layout style={styles.mainContainer}>
-      <View style={styles.statusContainer}>
-        <Text style={styles.statusText}>As of {todayDate},</Text>
-        <Text category='h5' style={styles.statusText}>
-          You have not found a teammate!
-        </Text>
-      </View>
-      <View style={styles.statusImage}>
-        <Image
-          style={styles.logo}
-          source={require('../../assets/images/orbital-logo.png')}
-        />
-      </View>
+    <Layout
+      style={[styles.mainContainer, !match ? styles.notFound : styles.found]}
+    >
+      {!match ? (
+        <>
+          <View style={styles.statusContainer}>
+            <Text style={styles.statusText}>As of {todayDate},</Text>
+            <Text category='h6' style={styles.statusText}>
+              You have not found a teammate!
+            </Text>
+          </View>
+          <View style={styles.statusImage}>
+            <Image
+              style={styles.logo}
+              source={require('../../assets/images/orbital-logo.png')}
+            />
+          </View>
+        </>
+      ) : (
+        <>
+          <View style={styles.matchedTextContainer}>
+            <Text category='h6' style={styles.matchedText}>
+              <Text>Teamed up with</Text>
+              {`\n${teammate.name}`}
+            </Text>
+          </View>
+          <View style={styles.matchingContainer}>
+            <View style={styles.matchedChild}>
+              {myImage.length > 0 ? (
+                <Avatar size='giant' source={{ uri: myImage }} />
+              ) : (
+                <UserAvatar name={name} size={56} fontSize={28} />
+              )}
+            </View>
+            <Connector />
+            <View style={styles.statusImage}>
+              <Image
+                style={styles.logo}
+                source={require('../../assets/images/orbital-logo.png')}
+              />
+            </View>
+            <Connector />
+            <View style={styles.matchedChild}>
+              {teamImage.length > 0 ? (
+                <Avatar size='giant' source={{ uri: teamImage }} />
+              ) : (
+                <UserAvatar name={teamName} size={56} fontSize={28} />
+              )}
+            </View>
+          </View>
+        </>
+      )}
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
   mainContainer: {
-    height: 108,
-    flexDirection: 'row',
     margin: 20,
     borderRadius: 10,
-    // backgroundColor: '#407BFF',
-    backgroundColor: '#FFB73A',
     shadowColor: 'rgba(0,0,0, .4)',
     shadowOffset: { height: 1, width: 1 },
     shadowOpacity: 1,
@@ -40,6 +111,14 @@ const styles = StyleSheet.create({
     elevation: 3,
     paddingVertical: 15,
     paddingHorizontal: 20
+  },
+  found: {
+    backgroundColor: '#B2D1FF'
+  },
+  notFound: {
+    height: 88,
+    flexDirection: 'row',
+    backgroundColor: '#FFB73A'
   },
   statusContainer: {
     width: '80%',
@@ -56,6 +135,31 @@ const styles = StyleSheet.create({
   logo: {
     width: 62,
     height: 62
+  },
+  matchingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  matchedChild: {
+    width: '20%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  matchedTextContainer: {
+    marginBottom: 10
+  },
+  matchedText: {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center'
+  },
+  connector: {
+    width: '14%',
+    borderBottomColor: '#407BFF',
+    borderBottomWidth: 3,
+    alignSelf: 'center',
+    marginHorizontal: 5
   }
 });
 
