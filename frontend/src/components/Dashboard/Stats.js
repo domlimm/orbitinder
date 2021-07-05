9d7ab0bdfc164375ef4c30baaae0c47899768946
domsterthebot <dominiclimkj3448@gmail.com>
2021-07-05 21:15:54 +0800

    Added liked by card

--- frontend/src/components/Dashboard/Stats.js
+++ frontend/src/components/Dashboard/Stats.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Dimensions, Platform } from 'react-native';
-import { Layout, Text } from '@ui-kitten/components';
+import {
+  TouchableNativeFeedback,
+  StyleSheet,
+  View,
+  Dimensions,
+  Platform
+} from 'react-native';
+import { Layout, Text, Tooltip } from '@ui-kitten/components';
 import { useSelector } from 'react-redux';
 import { Feather } from '@expo/vector-icons';
 
@@ -9,6 +15,8 @@ const { width, height } = Dimensions.get('window');
 const Stats = () => {
   const [likesCount, setLikesCount] = useState('');
   const [dislikesCount, setDislikesCount] = useState('');
+  const [likedByCount, setLikedByCount] = useState('');
+  const [visible, setVisible] = useState(false);
   const userData = useSelector(state => state.user.userData);
 
   useEffect(() => {
@@ -18,6 +26,10 @@ const Stats = () => {
 
     if (userData.dislikes !== undefined) {
       setDislikesCount(userData.dislikes.length);
+    }
+
+    if (userData.likedBy !== undefined) {
+      setLikedByCount(userData.likedBy.length);
     }
   }, [userData]);
 
@@ -51,11 +63,37 @@ const Stats = () => {
     </Layout>
   );
 
+  const LikedByCard = () => (
+    <TouchableNativeFeedback
+      onPress={() => setVisible(true)}
+      background={TouchableNativeFeedback.Ripple('#00000020', false)}
+      useForeground={true}
+    >
+      <Layout style={styles.likedByContainer}>
+        <Text category='h6'>
+          {likedByCount.length === 0
+            ? 'No user has swiped right on you just yet!'
+            : likedByCount.length === 1
+            ? `${likedByCount} user has swiped right on you!`
+            : `${likedByCount} users have swiped right on you!`}
+        </Text>
+      </Layout>
+    </TouchableNativeFeedback>
+  );
+
   return (
     <View style={styles.parentContainer}>
       <Text category='h5' style={styles.headerTitle}>
         Personal Insights
       </Text>
+      <Tooltip
+        anchor={LikedByCard}
+        visible={visible}
+        onBackdropPress={() => setVisible(false)}
+        placement='bottom'
+      >
+        Test
+      </Tooltip>
       <View style={styles.statCardsContainer}>
         <StatCard type='likes' />
         <StatCard type='dislikes' />
@@ -126,6 +164,19 @@ const styles = StyleSheet.create({
   icon: {
     height: 32,
     width: 32
+  },
+  likedByContainer: {
+    width: '100%',
+    marginBottom: 16,
+    borderRadius: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    shadowColor: 'rgba(0,0,0, .4)',
+    shadowOffset: { height: 1, width: 1 },
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    elevation: 3
   }
 });
 
