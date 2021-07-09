7be930be445656a54b4b35d5d73faa930051b2d0
domsterthebot <dominiclimkj3448@gmail.com>
2021-07-09 14:47:36 +0800

    Added notifications and routing for messages

--- frontend/src/navigation/RootNavigation.js
+++ frontend/src/navigation/RootNavigation.js
@@ -3,7 +3,6 @@ import React from 'react';
 export const navigationRef = React.createRef();
 
 const navigate = (name, params) => {
-  console.log('entered navigating');
   navigationRef.current?.navigate(name, params);
 };
 
