cadf474386e3354b42e4a492c7d460797bd3ad94
domsterthebot <dominiclimkj3448@gmail.com>
2021-07-23 12:11:58 +0800

    Fixed bug where requests overview screen flatlist throws an error when scrolling with empty requests data

--- frontend/src/screens/Main/RequestsScreen.js
+++ frontend/src/screens/Main/RequestsScreen.js
@@ -52,11 +52,17 @@ const RequestsScreen = () => {
               key={index}
               activeOpacity={0.8}
               onPress={() => {
-                setTypeIndex(index);
-                flatListRef.current.scrollToOffset({
-                  animated: true,
-                  offset: 0
-                });
+                if (
+                  allRequests.length > 0 ||
+                  activeRequests.length > 0 ||
+                  sentRequests.length > 0
+                ) {
+                  setTypeIndex(index);
+                  flatListRef.current.scrollToOffset({
+                    animated: true,
+                    offset: 0
+                  });
+                }
               }}
             >
               <Text
